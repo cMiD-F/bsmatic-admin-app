103cf767a6d92fe5d5c4cdc11374a8b6743cbc27
Cauet <cauet.cmd@gmail.com>
2023-06-21 15:39:52 -0300

    integ-v0.8

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -5,12 +5,13 @@ const getTokenFromLocalStorage = localStorage.getItem("user")
   ? JSON.parse(localStorage.getItem("user"))
   : null;
 
-const config = {
-  headers: {
-    Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
-    Accept: "application/json",
-  }
-};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${getTokenFromLocalStorage?.token}`,
+      Accept: "application/json",
+    }
+  };
+  
 
 const login = async (user) => {
   const response = await axios.post(`${base_url}user/login-admin`, user);
